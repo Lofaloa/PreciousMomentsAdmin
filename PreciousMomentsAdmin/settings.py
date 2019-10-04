75718b85cae7eafe64eee75f0af302dea7c7f0ad
Logan Farci <farci.logan@gmail.com>
2019-10-05 01:21:26 +0200

    index.html: the grid of cards is now center justified

--- PreciousMomentsAdmin/settings.py
+++ PreciousMomentsAdmin/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'PreciousMomentsAdmin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'glasses',
+        'USER':'logan',
+        'PASSWORD':'rootroot'
     }
 }
 
--- glasses/views.py
+++ glasses/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Glass
+
 def index(request):
-    return render(request, 'glasses/index.html')
+    glasses = Glass.objects.all()
+    return render(request, 'glasses/index.html', {'glasses': glasses})
