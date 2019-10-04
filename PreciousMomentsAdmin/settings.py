4ca82d40d9184a75dad15b28c4df3b31b91d3e2f
Logan Farci <farci.logan@gmail.com>
2019-10-04 22:32:05 +0200

    index.html: shop home page theme template added

--- PreciousMomentsAdmin/settings.py
+++ PreciousMomentsAdmin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'glasses.apps.GlassesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
