d209b40604cbb97d52aaca7d3d60fb808a59764a
Logan Farci <farci.logan@gmail.com>
2019-10-05 10:02:14 +0200

    index.html: glass cards show a picture of the glass

--- glasses/models.py
+++ glasses/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
+import os
 
 class Glass(models.Model):
     name = models.CharField(max_length=50)
     amount = models.PositiveIntegerField()
     price = models.FloatField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
