3b8ad29345e801c19d1d8becebf5a9a6fd61a7be
Logan Farci <farci.logan@gmail.com>
2019-10-05 10:53:06 +0200

    details.html: the glass picture and other fields are shown

--- glasses/urls.py
+++ glasses/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    # ex: /glasses/
     path('', views.index, name='index'),
+    # ex: /glasses/5
+    path('<int:glass_id>/', views.detail, name='detail'),
 ]
 
 if settings.DEBUG:
--- glasses/views.py
+++ glasses/views.py
@@ -6,3 +6,7 @@ from .models import Glass
 def index(request):
     glasses = Glass.objects.all()
     return render(request, 'glasses/index.html', {'glasses': glasses})
+
+def detail(request, glass_id):
+    glass = Glass.objects.get(pk=glass_id)
+    return render(request, 'glasses/detail.html', {'glass': glass})
