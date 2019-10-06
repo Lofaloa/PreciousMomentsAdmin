bc9977e913f6c88367ca1fa5a35cf7b11b35200f
Logan Farci <farci.logan@gmail.com>
2019-10-06 17:25:39 +0200

    GlassFormModel: unit tests added

--- glasses/models.py
+++ glasses/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinValueValidator
 import os
 
 class Glass(models.Model):
-    name = models.CharField(max_length=50)
-    amount = models.PositiveIntegerField()
-    price = models.FloatField()
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    amount = models.PositiveIntegerField(null=False, blank=True)
+    price = models.FloatField(null=False, validators=[MinValueValidator(0.0)])
+    image = models.ImageField(upload_to='images/', blank=True, null=False)
 
     def __str__(self):
         return self.name
---
+++ glasses/tests/tests_glass_form.py
@@ -0,0 +1,90 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+from django.conf import settings
+from glasses.forms import GlassModelForm
+import os
+
+class GlassModelFormTests(TestCase):
+
+    def create_test_image(self):
+        """
+        Creates an image used to simulate an image uploaded by a user.
+        """
+        image_path = os.path.join(settings.MEDIA_ROOT, 'images/test_image.png')
+        return SimpleUploadedFile(
+            name='test_image.png',
+            content=open(image_path, 'rb').read(),
+            content_type='image/png'
+        )
+
+    def create_glass_form(self, name, amount, price):
+        """
+        Creates a form with the specified data and a test image.
+        """
+        form_data = {
+            'name': name,
+            'amount': amount,
+            'price': price,
+            'image': self.create_test_image()
+        }
+        return GlassModelForm(form_data)
+
+    def test_image_creation(self):
+        image= self.create_test_image()
+        self.assertIsNotNone(image)
+
+    def test_valid_data(self):
+        """
+        The form should be valid when all the specified data are valid.
+        """
+        form = self.create_glass_form(
+            'Verre à tournesol',
+            '5',
+            '4.99'
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_null_glass_name(self):
+        """
+        The form should not be valid when the glass name is null (None).
+        """
+        form = self.create_glass_form(
+            None,
+            '5',
+            '4.99'
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_blank_glass_name(self):
+        """
+        The form should not be valid when the glass name is blank.
+        """
+        form = self.create_glass_form(
+            '',
+            '5',
+            '4.99'
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_negative_amount(self):
+        """
+        The form should not be valid when the amount of glasses is negative.
+        """
+        form = self.create_glass_form(
+            'Verre à tournesol',
+            '-5',
+            '4.99'
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_negative_price(self):
+        """
+        The form should not be valid when the price of a glass is negative.
+        """
+        form = self.create_glass_form(
+            'Verre à tournesol',
+            '5',
+            '-4.99'
+        )
+        self.assertFalse(form.is_valid())
+
