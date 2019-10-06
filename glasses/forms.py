74054579d3560be40989643570c65287c196ca9f
Logan Farci <farci.logan@gmail.com>
2019-10-06 18:22:35 +0200

    add.html: labels and help texts updated

--- glasses/forms.py
+++ glasses/forms.py
@@ -5,3 +5,15 @@ class GlassModelForm(forms.ModelForm):
     class Meta:
         model = Glass
         fields = ('name', 'amount', 'price', 'image')
+        labels = {
+            "name": "Nom personnalisé",
+            "amount": "Nombre de verres disponibles",
+            "price": "Prix de vente",
+            "image": "Photo du verre",
+        }
+        help_texts = {
+            'name': 'Choisissez un nom unique qui correspond à votre réalisation',
+            'amount': 'La quantié doit être positive',
+            'price': 'Le prix doit être positif',
+            'image': 'Choisissez une photo qui représente votre réalisation',
+        }
