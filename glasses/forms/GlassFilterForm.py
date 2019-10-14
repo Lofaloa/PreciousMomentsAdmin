d7e7ae6a667aaa7513431f32fb983347d2281a7b
Logan Farci <farci.logan@gmail.com>
2019-10-14 12:23:21 +0200

    It is now possible to search glasses by name

---
+++ glasses/forms/GlassFilterForm.py
@@ -0,0 +1,6 @@
+from django import forms
+from glasses.models import Glass
+
+class GlassFilterForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    
--- glasses/views/GlassListView.py
+++ glasses/views/GlassListView.py
@@ -4,3 +4,10 @@ from glasses.models import Glass
 class GlassListView(ListView):
     model = Glass
     context_object_name = 'glasses'
+
+    def get_queryset(self):
+        selection = self.request.GET.get('name')
+        if selection:
+            return Glass.objects.filter(name__contains=selection)
+        else:
+            return Glass.objects.all()
