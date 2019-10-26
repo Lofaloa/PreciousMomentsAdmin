cf663f1a897ec79a9d165e7ceb23ac8f34b2a526
Lofaloa <farci.logan@gmail.com>
2019-10-26 16:48:00 +0200

    Views class related to glasses are now in glasses subfolder

--- glasses/urls.py
+++ glasses/urls.py
@@ -3,24 +3,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from glasses.views.IndexView import IndexView
-from glasses.views.GlassListView import GlassListView
-from glasses.views.GlassDetailView import GlassDetailView
-from glasses.views.GlassCreateView import GlassCreateView
-from glasses.views.GlassDeleteView import GlassDeleteView
-from glasses.views.GlassUpdateView import GlassUpdateView
+from glasses.views.glasses.GlassListView import GlassListView
+from glasses.views.glasses.GlassDetailView import GlassDetailView
+from glasses.views.glasses.GlassCreateView import GlassCreateView
+from glasses.views.glasses.GlassDeleteView import GlassDeleteView
+from glasses.views.glasses.GlassUpdateView import GlassUpdateView
 
 urlpatterns = [
     # example: /
     path('', IndexView.as_view(), name="index"),
     # example: glasses/
     path('glasses/', GlassListView.as_view(), name="glass_list"),
-    # example: glasses/5    
+    # example: glasses/5
     path('<int:pk>/', GlassDetailView.as_view(), name="glass_detail"),
-    # example: glasses/create    
+    # example: glasses/create
     path('create/', GlassCreateView.as_view(), name="glass_create"),
-    # example: glasses/5/delete    
+    # example: glasses/5/delete
     path('<int:pk>/delete', GlassDeleteView.as_view(), name="glass_delete"),
-    # example: glasses/5/update    
+    # example: glasses/5/update
     path('<int:pk>/update', GlassUpdateView.as_view(), name="glass_update"),
 ]
 
